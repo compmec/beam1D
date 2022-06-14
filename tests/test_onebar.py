dbf2d38918342fc40a55cd492458758a060aa822
Carlos Adir <carlos.adir.leite@gmail.com>
2022-06-14 09:33:05 -0300

    Add tests of torsion in a rod, Euler beam

--- tests/test_onebar.py
+++ tests/test_onebar.py
@@ -158,6 +158,162 @@ def test_tractionR():
 		np.testing.assert_almost_equal(Utest, Ugood)
 		np.testing.assert_almost_equal(Ftest, Fgood)
 
+@pytest.mark.timeout(2)
+def test_torsionXcircle():
+	ntests = 10
+	for i in range(ntests):
+		E = 1 + np.random.rand()
+		nu = 0.49*np.random.rand()
+		d = 1 + np.random.rand()
+		L = 1 + np.random.rand()
+		T = 2*np.random.rand()-1
+
+		steel = Isotropic(E=E, nu=nu)
+		circle = Circle(R=d/2, nu=nu)
+		A = (0, 0, 0)
+		B = (L, 0, 0)
+		bar = EulerBernoulli(A, B)
+		bar.material = steel
+		bar.section = circle
+
+		U = np.empty((2, 6), dtype="object")
+		F = np.zeros((2, 6))
+
+		# U[0, 3] = 0
+		U[0, :] = 0
+		F[1, 3] = T
+
+		K = bar.stiffness_matrix()
+		Utest, Ftest = solve(K, F, U)
+
+
+		tx = 64*T*L*(1+nu)/(np.pi*E*d**4)
+		Ugood = np.array([[0, 0, 0, 0, 0, 0],
+						  [0, 0, 0, tx, 0, 0]])
+		Fgood = np.array([[0, 0, 0, -T, 0, 0],
+						  [0, 0, 0, T, 0, 0]])
+
+		np.testing.assert_almost_equal(Utest, Ugood)
+		np.testing.assert_almost_equal(Ftest, Fgood)	
+
+@pytest.mark.timeout(2)
+def test_torsionYcircle():
+	ntests = 10
+	for i in range(ntests):
+		E = 1 + np.random.rand()
+		nu = 0.49*np.random.rand()
+		d = 1 + np.random.rand()
+		L = 1 + np.random.rand()
+		T = 2*np.random.rand()-1
+
+		steel = Isotropic(E=E, nu=nu)
+		circle = Circle(R=d/2, nu=nu)
+		A = (0, 0, 0)
+		B = (0, L, 0)
+		bar = EulerBernoulli(A, B)
+		bar.material = steel
+		bar.section = circle
+
+		U = np.empty((2, 6), dtype="object")
+		F = np.zeros((2, 6))
+
+		# U[0, 4] = 0
+		U[0, :] = 0
+		F[1, 4] = T
+
+		K = bar.stiffness_matrix()
+		Utest, Ftest = solve(K, F, U)
+
+
+		ty = 64*T*L*(1+nu)/(np.pi*E*d**4)
+		Ugood = np.array([[0, 0, 0, 0, 0, 0],
+						  [0, 0, 0, 0, ty, 0]])
+		Fgood = np.array([[0, 0, 0, 0, -T, 0],
+						  [0, 0, 0, 0, T, 0]])
+
+		np.testing.assert_almost_equal(Utest, Ugood)
+		np.testing.assert_almost_equal(Ftest, Fgood)
+
+@pytest.mark.timeout(2)
+def test_torsionZcircle():
+	ntests = 10
+	for i in range(ntests):
+		E = 1 + np.random.rand()
+		nu = 0.49*np.random.rand()
+		d = 1 + np.random.rand()
+		L = 1 + np.random.rand()
+		T = 2*np.random.rand()-1
+
+		steel = Isotropic(E=E, nu=nu)
+		circle = Circle(R=d/2, nu=nu)
+		A = (0, 0, 0)
+		B = (0, 0, L)
+		bar = EulerBernoulli(A, B)
+		bar.material = steel
+		bar.section = circle
+
+		U = np.empty((2, 6), dtype="object")
+		F = np.zeros((2, 6))
+
+		# U[0, 5] = 0
+		U[0, :] = 0
+		F[1, 5] = T
+
+		K = bar.stiffness_matrix()
+		Utest, Ftest = solve(K, F, U)
+
+
+		tz = 64*T*L*(1+nu)/(np.pi*E*d**4)
+		Ugood = np.array([[0, 0, 0, 0, 0, 0],
+						  [0, 0, 0, 0, 0, tz]])
+		Fgood = np.array([[0, 0, 0, 0, 0, -T],
+						  [0, 0, 0, 0, 0, T]])
+
+		np.testing.assert_almost_equal(Utest, Ugood)
+		np.testing.assert_almost_equal(Ftest, Fgood)
+
+@pytest.mark.timeout(2)
+def test_torsionRcircle():
+	ntests = 10
+	for i in range(ntests):
+		r = np.random.rand(3)  # random direction
+		r = np.array([1, 1, 0], dtype="float64")
+		r /= np.linalg.norm(r)  # To r a unit vector  
+		E = 1 + np.random.rand()
+		nu = 0.49*np.random.rand()
+		d = 1 + np.random.rand()
+		L = 2*np.random.rand()-1
+		T = 2*np.random.rand()-1
+
+		steel = Isotropic(E=E, nu=nu)
+		circle = Circle(R=d/2, nu=nu)
+		A = (0, 0, 0)
+		B = L*r
+		bar = EulerBernoulli(A, B)
+		bar.material = steel
+		bar.section = circle
+
+		U = np.empty((2, 6), dtype="object")
+		F = np.zeros((2, 6))
+
+		# U[0, 3:] = 0
+		U[0, :] = 0
+		F[1, 3:] = T * r
+
+		K = bar.stiffness_matrix()
+		Utest, Ftest = solve(K, F, U)
+
+		tr = 64*T*L*(1+nu)/(np.pi*E*d**4)
+		Ugood = np.zeros((2, 6))
+		Fgood = np.zeros((2, 6))
+		Ugood[1, 3:] = tr * r
+		Fgood[0, 3:] = -T * r
+		Fgood[1, 3:] = T * r
+
+		np.testing.assert_almost_equal(Utest, Ugood)
+		np.testing.assert_almost_equal(Ftest, Fgood)
+
+
 
 if __name__ == "__main__":
 	pytest.main()
