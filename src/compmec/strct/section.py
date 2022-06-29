b6ad3a3585aa5ff17b635de2496b7fc234f84cd8
Carlos Adir <carlos.adir.leite@gmail.com>
2022-06-29 08:46:31 -0300

    Separate computations of area and inertia in functions

--- src/compmec/strct/section.py
+++ src/compmec/strct/section.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygmsh
 from compmec.strct.__classes__ import Section
 
 
@@ -14,51 +15,170 @@ class Retangular(Section):
 		h is the height
 		"""
 		super().__init__()
-		self.Ax = b*h
-		k = 10*(1+nu)/(12+11*nu)
+		self.nu = nu
+		self.b = b
+		self.h = h
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
+		k = 10*(1+self.nu)/(12+11*self.nu)
+		self.Ax = self.b*self.h
 		self.Ay = k * self.Ax
 		self.Az = k * self.Ax
-		self.Ix = b*h*(b**2 + h**2)/12  # Wrong, not finished
-		self.Iy = b*h**3/12
-		self.Iz = b**3 * h/12
 
-class ThinHollowSquare(Section):
+	def __compute_inertias(self):
+		self.Iy = self.b*self.h**3/12
+		self.Iz = self.h*self.b**3/12
+		raise NotImplementedError("Torsion for a retangular is not defined yet")
+
+class HollowRetangular(Section):
+	def __init__(self, be: float, he:float, bi:float, hi:float, nu:float):
+		"""
+		b is the lower base
+		h is the height
+		"""
+		super().__init__()
+		self.nu = nu
+		self.be = be
+		self.he = he
+		self.bi = bi
+		self.hi = hi
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
+		raise NotImplementedError("Areas for hollow retangular still not defined")
+
+	def __compute_inertias(self):
+		raise NotImplementedError("Inertias for hollow retangular still not defined")
+
+class ThinRetangular(Section):
+	def __init__(self, b: float, h:float, nu:float):
+		"""
+		b is the lower base
+		h is the height
+		"""
+		super().__init__()
+		self.nu = nu
+		self.b = b
+		self.h = h
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
+		raise NotImplementedError("Areas for thin retangular still not defined")
+
+	def __compute_inertias(self):
+		raise NotImplementedError("Inertias for thin retangular still not defined")
+
+class Square(Section):
 	def __doc__(self):
 		"""
 		Docs
 		"""
-	def __init__(self, side:float, nu:float):
+	def __init__(self, b:float, nu:float):
 		super().__init__()
+		self.nu = nu
+		self.b = b
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
 		k = 20*(1+nu)/(4+3*nu)
+		raise NotImplementedError("Areas for a square are not defined")
+
+	def __compute_inertias(self):
+		raise NotImplementedError("Inertias for a square are not defined")
+
+class HollowSquare(Section):
+	def __doc__(self):
+		"""
+		Docs
+		"""
+	def __init__(self, be:float, bi:float, nu:float):
+		super().__init__()
+		self.nu = nu
+		self.be = be
+		self.bi = bi
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
+		raise NotImplementedError("Areas for a hollow square are not defined")
+
+	def __compute_inertias(self):
+		raise NotImplementedError("Inertias for a hollow square are not defined")
+
+class ThinSquare(Section):
+	def __doc__(self):
+		"""
+		Docs
+		"""
+	def __init__(self, b:float, nu:float):
+		super().__init__()
+		self.nu = nu
+		self.b = b
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
+		raise NotImplementedError("Areas for a hollow square are not defined")
+
+	def __compute_inertias(self):
+		raise NotImplementedError("Inertias for a Thin square are not defined")
 
 class Circle(Section):
 	def __init__(self, R:float, nu:float):
 		super().__init__()
-		self.Ax = np.pi* R**2
-		k = 6*(1+nu)/(7+6*nu)
+		self.nu = nu
+		self.R = R
+		self.__compute_areas()
+		self.__compute_inertias()
+
+	def __compute_areas(self):
+		k = 6*(1+self.nu)/(7+6*self.nu)
+		self.Ax = np.pi* self.R**2
 		self.Ay = k * self.Ax
 		self.Az = k * self.Ax
-		d = 2 * R
-		self.Ix = np.pi * d**4 / 32
-		self.Iy = np.pi * d**4 / 64
-		self.Iz = np.pi * d**4 / 64 
+	
+	def __compute_inertias(self):
+		R4 = self.R**4
+		self.Ix = np.pi * R4 / 2
+		self.Iy = np.pi * R4 / 4
+		self.Iz = np.pi * R4 / 4
+	
+	def triangle_mesh(self, size:float):
+		with pygmsh.geo.Geometry() as geom:
+			R = np.sqrt(self.Ax/np.pi)
+			geom.add_circle((0, 0), R, mesh_size=size)
+			mesh = geom.generate_mesh()
+		return mesh
 
 
 class HollowCircle(Section):
 	def __init__(self, Ri:float, Re:float, nu:float):
 		super().__init__()
-		m = Ri/Re
-		k = 6*(1+nu)*((1+m**2)**2)/( (7+6*nu)*(1+m**2)**2 + (20+12*nu)*m**2)
+		self.nu = nu
+		self.Ri = Ri
+		self.Re = Re
+		self.__compute_areas()
+		self.__compute_inertias()
 
-		self.Ax = np.pi* (Re**2-Ri**2)
-		k = 6*(1+nu)/(7+6*nu)
+	def __compute_areas(self):
+		m = self.Ri/self.Re
+		nu = self.nu
+		k = 6*(1+nu)*((1+m**2)**2)/( (7+6*nu)*(1+m**2)**2 + (20+12*nu)*m**2)
+		self.Ax = np.pi* self.Re**2 *(1-m**2)
 		self.Ay = k * self.Ax
 		self.Az = k * self.Ax
-		di = 2 * Ri
-		de = 2 * Re
-		self.Ix = np.pi * (de**4-di**4) / 32
-		self.Iy = np.pi * (de**4-di**4) / 64
-		self.Iz = np.pi * (de**4-di**4) / 64 
+
+	def __compute_inertias(self):
+		Ri4 = self.Ri**4
+		Re4 = self.Re**4
+		self.Ix = np.pi * (Re4 - Ri4) / 2
+		self.Iy = np.pi * (Re4 - Ri4) / 4
+		self.Iz = np.pi * (Re4 - Ri4) / 4
 
 class ThinCircle(Section):
 	"""
@@ -66,14 +186,23 @@ class ThinCircle(Section):
 	"""
 	def __init__(self, R:float, nu:float):
 		super().__init__()
-		e = 0.01 * R
-		k = 2*(1+nu)/(4+3*nu)
-		self.Ax = 2 * np.pi * R * e
+		self.nu = nu
+		self.R = R
+		self.e = 0.01*self.R
+		self.__compute_areas()
+		self.__compute_inertias
+
+	def __compute_areas(self):
+		k = 2*(1+self.nu)/(4+3*self.nu)
+		self.Ax = 2 * np.pi * self.R * self.e
 		self.Ay = k * self.Ax
 		self.Az = k * self.Ax
-		self.Ix = 2 * np.pi*e * R**3
-		self.Iy = np.pi * e * R**3
-		self.Iz = np.pi * e * R**3
+	
+	def __compute_inertias(self):
+		eR3 = self.e * self.R**3
+		self.Ix = 2 * np.pi * eR3
+		self.Iy = np.pi * eR3
+		self.Iz = np.pi * eR3
 
 class PerfilI(Section):
 	def __init__(self, b:float, h:float, t1:float, t2:float):
