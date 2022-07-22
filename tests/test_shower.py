079c03bceb2320ec30845f46d7251a99b48205f9
Carlos Adir <carlos.adir.leite@gmail.com>
2022-07-22 11:23:31 -0300

    Update a test to show images
    It's not exactly a test, but it should be more an example. Needs show colored graphs

---
+++ tests/test_shower.py
@@ -0,0 +1,63 @@
+from compmec.strct.beam import EulerBernoulli
+from compmec.strct.section import Circle, Square
+from compmec.strct.material import Isotropic
+from compmec.strct.system import StaticSystem
+from compmec.strct.shower import ShowerStaticSystem
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def tst_example09():
+    pass
+
+def tst_example20():
+    A = (0, 0, 0)
+    B = (1000, 500, 0)
+    C = (1000, -500, 0)
+    D = (1000, 0, 500)
+    beamAB = EulerBernoulli([A, B])
+    beamAC = EulerBernoulli([A, C])
+    beamAD = EulerBernoulli([A, D])
+    beamBC = EulerBernoulli([B, C])
+    beamBD = EulerBernoulli([B, D])
+    beamCD = EulerBernoulli([C, D])
+    circle = Circle(R=8/2, nu=0.3)
+    steel = Isotropic(E=210e+3, nu=0.3)
+    system = StaticSystem()
+    for beam in [beamAB, beamAC, beamAD, beamBC, beamBD, beamCD]:
+        for t in np.linspace(0, 1, 21):
+            beam.path(t)
+        beam.material = steel
+        beam.section = circle
+        system.add_element(beam)
+    E = beamBC.path(0.5)
+    system.add_BC(A, {"ux": 0,
+                    "uy": 0,
+                    "uz": 0,
+                    "tx": 0,
+                    "ty": 0,
+                    "tz": 0})
+    system.add_BC(B, {"uz": 0})
+    system.add_BC(C, {"uz": 0})
+    system.add_load(D, {"Fz": 5000000})
+    system.run()
+
+    shower = ShowerStaticSystem(system)
+
+    plt.figure()
+    ax = plt.gca()
+    shower.plot2D(projector='xy', deformed=False, axes=ax)
+    shower.plot2D(projector='xy', deformed=True, axes=ax)
+    plt.figure()
+    ax = plt.axes(projection='3d')
+    shower.plot3D(deformed=False, axes=ax)
+    shower.plot3D(deformed=True, axes=ax)
+    plt.legend()
+    plt.show()
+
+
+def main():
+    tst_example20()
+
+if __name__ == "__main__":
+    main()
