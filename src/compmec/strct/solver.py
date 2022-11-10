Correct test_solver due to numeric precision error

src/compmec/strct/solver.py
@@ -9,7 +9,7 @@ In the end we have a linear system which is given by
 
 """
 
-def solve(K : np.ndarray, F : np.ndarray, U:np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def solve(K : np.ndarray, F : np.ndarray, U:np.ndarray, TOLERANCE=1e-9) -> Tuple[np.ndarray, np.ndarray]:
     """
     K is a big matrix of shape (npts, 6, npts, 6)
     F is a matrix of shape (npts, 6)
@@ -54,7 +54,7 @@ def solve(K : np.ndarray, F : np.ndarray, U:np.ndarray) -> Tuple[np.ndarray, np.
     try:
         Uu = la.solve(Kuu, B)
     except np.linalg.LinAlgError as e:
-        Uu = la.lstsq(Kuu, B, rcond=1e-10)[0]
+        Uu = la.lstsq(Kuu, B, rcond=TOLERANCE)[0]
         
     Fu = Kkk @ Uk + Kku @ Uu
 
@@ -64,7 +64,7 @@ def solve(K : np.ndarray, F : np.ndarray, U:np.ndarray) -> Tuple[np.ndarray, np.
     for i, (a, b) in enumerate(unknown):
         U[a, b] = Uu[i]
     U = np.array(U, dtype="float64")
-    U[np.abs(U) < 1e-10] = 0
+    U[np.abs(U) < TOLERANCE] = 0
     
     return U, F
 

src/compmec/strct/system.py
@@ -385,21 +385,21 @@ class StaticSystem():
             p = element.path(t)
             self._geometry.index_point_at(p)
 
-    def __mount_U(self) -> np.ndarray:
+    def mount_U(self) -> np.ndarray:
         npts = self._geometry.npts
         U = np.empty((npts, 6), dtype="object")
         for index, position, displacement in self._boundarycondition.bcvals:
             U[index, position] = displacement
         return U
 
-    def __mount_F(self) -> np.ndarray:
+    def mount_F(self) -> np.ndarray:
         npts = self._geometry.npts
         F = np.zeros((npts, 6))
         for index, position, loads in self._loads.loads:
             F[index, position] += loads
         return F
 
-    def __mount_K(self) -> np.ndarray:
+    def mount_K(self) -> np.ndarray:
         npts = self._geometry.npts
         K = np.zeros((npts, 6, npts, 6))
         for element in self._structure.elements:
@@ -417,9 +417,9 @@ class StaticSystem():
     def run(self):
         for element in self._structure.elements:
             self.__getpointsfrom(element)
-        K = self.__mount_K()
-        F = self.__mount_F()
-        U = self.__mount_U()
+        K = self.mount_K()
+        F = self.mount_F()
+        U = self.mount_U()
         U, F = solve(K, F, U)
         self._solution = U
         self.apply_on_elements()

tests/test_examples.py
@@ -7,7 +7,7 @@ from compmec.strct.system import StaticSystem
 from matplotlib import pyplot as plt
 
 @pytest.mark.order(10)
-@pytest.mark.skip(reason="Must implement the functions to get displacements, momentums and etc from the beam")
+# @pytest.mark.skip(reason="Must implement the functions to get displacements, momentums and etc from the beam")
 @pytest.mark.dependency(
 	depends=["tests/test_onerodallcharges.py::test_end",
              "tests/test_bendingretangular.py::test_end"],
@@ -70,6 +70,7 @@ def test_example3():
     beamAB = EulerBernoulli([A, B])
     beamAB.section = Circle(R=8/2, nu=0.3)
     beamAB.material = Isotropic(E=210e+3, nu=0.3)
+    beamAB.addt(0.6)
     C = beamAB.path(0.6)
     system = StaticSystem()
     system.add_element(beamAB)
@@ -84,10 +85,6 @@ def test_example3():
     Ugood[1, 5] = -32*10*(600**2)/(np.pi* 210e+3 * 8**4)
     Ugood[2, 5] = Ugood[1, 5]
     Ugood[2, 1] = Ugood[1, 1] + 400 * Ugood[1, 5]
-    print("Usolu = ")
-    print(Usolu)
-    print("Ugood = ")
-    print(Ugood)
     np.testing.assert_almost_equal(Usolu, Ugood)
 
 
@@ -102,6 +99,8 @@ def test_example4():
     beamAB.material = Isotropic(E=210e+3, nu=0.3)
     C = beamAB.path(0.3)
     D = beamAB.path(0.7)
+    beamAB.addt(0.3)
+    beamAB.addt(0.7)
     system = StaticSystem()
     system.add_element(beamAB)
     system.add_BC(A, {"ux": 0,
@@ -234,8 +233,8 @@ def test_example9():
 @pytest.mark.timeout(6)
 @pytest.mark.dependency(depends=["test_example9"])
 def test_example10():
-    A = (300, 0)
-    B = (0, 500)
+    A = (300, 0, 0)
+    B = (0, 500, 0)
     C = (300, 500, 0)
     beamAC = EulerBernoulli([A, C])
     beamBC = EulerBernoulli([B, C])

tests/test_solver.py
@@ -13,6 +13,7 @@ def test_begin():
 @pytest.mark.timeout(2)
 @pytest.mark.dependency(depends=["test_begin"])
 def test_cantilever2pts():
+    TOLERANCE = 1e-6
     Korig = [[[[ 10555.7513,  0,  0,  0, 0,  0],
                [-10555.7513,  0,  0,  0, 0,  0]],
               [[ 0,  0.506676063,  0,  0, 0,  253.338032],
@@ -49,8 +50,8 @@ def test_cantilever2pts():
     Fgood = [[0, 10, 0, 0, 0, 10000],
              [0, -10, 0, 0, 0, 0]]
     Utest, Ftest = solve(Korig, Forig, Uorig)
-    np.testing.assert_almost_equal(Utest, Ugood, decimal=6)
-    np.testing.assert_almost_equal(Ftest, Fgood, decimal=6)
+    np.testing.assert_almost_equal(Utest, Ugood, decimal=int(-np.log10(TOLERANCE)))
+    np.testing.assert_almost_equal(Ftest, Fgood, decimal=int(-np.log10(TOLERANCE)))
 
 
 @pytest.mark.order(1)