82b330cddb659ae3c63e27a160705ce2dcfafdaf
CarlosAdir <carlos.adir.leite@gmail.com>
2022-11-26 10:46:28 -0300

    Move circular bending tests for another file

--- tests/test_one_rod_beam_charges.py
+++ tests/test_one_rod_beam_charges.py
@@ -30,9 +30,6 @@ class InitRodBeamEuler(object):
     ):
         if not isinstance(directionstr, str):
             raise TypeError
-        if perpendicular_to is not None:
-            if not isinstance(perpendicular_to, str):
-                raise TypeError
         vector = np.random.uniform(-1, 1, 3)
         for i, s in enumerate(["x", "y", "z"]):
             if s not in directionstr:
@@ -293,12 +290,275 @@ class TestOneRodBeamTorsion(InitRodBeamEuler):
         pass
 
 
+class TestOneRodBeamBendingForce(InitRodBeamEuler):
+    def compute_analitic_displacement_field(self):
+        Iy = self.section.I[1]
+        P = self.charge
+        L = self.lenght
+        E = self.material.E
+        return P * L**3 / (3 * E * Iy)
+
+    def compute_analitic_rotation_field(self):
+        Iy = self.section.I[1]
+        P = self.charge
+        L = self.lenght
+        E = self.material.E
+        return P * L**2 / (2 * E * Iy)
+
+    def run_test(self):
+        self.set_random_beam()
+        self.charge = np.random.uniform(-1, 1)
+
+        P = self.charge
+        L = self.lenght
+        U = np.empty((2, 6), dtype="object")
+        F = np.zeros((2, 6))
+        U[0, :] = 0
+        F[1, :3] = P * self.direction_force
+
+        K = self.beam.stiffness_matrix()
+        Utest, Ftest = solve(K, F, U)
+
+        uv = self.compute_analitic_displacement_field()
+        tw = self.compute_analitic_rotation_field()
+        direction_momentum = np.cross(self.direction_beam, self.direction_force)
+        direction_momentum /= np.linalg.norm(direction_momentum)
+        Ugood = np.zeros((2, 6))
+        Fgood = np.zeros((2, 6))
+        Ugood[1, :3] = uv * self.direction_force
+        Ugood[1, 3:] = tw * direction_momentum
+        Fgood[0, :3] = -P * self.direction_force
+        Fgood[0, 3:] = -P * L * direction_momentum
+        Fgood[1, :3] = P * self.direction_force
+
+        np.testing.assert_almost_equal(Utest, Ugood)
+        np.testing.assert_almost_equal(Ftest, Fgood)
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(depends=["test_begin"])
+    def test_random_x2y(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("x")
+            self.direction_force = self.get_random_unit_vector("y", self.direction_beam)
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=["test_begin", "TestOneRodBeamBendingForce::test_random_x2y"]
+    )
+    def test_random_x2z(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("x")
+            self.direction_force = self.get_random_unit_vector("z", self.direction_beam)
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=["test_begin", "TestOneRodBeamBendingForce::test_random_x2y"]
+    )
+    def test_random_y2x(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("y")
+            self.direction_force = self.get_random_unit_vector("x", self.direction_beam)
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=["test_begin", "TestOneRodBeamBendingForce::test_random_x2y"]
+    )
+    def test_random_y2z(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("y")
+            self.direction_force = self.get_random_unit_vector("z", self.direction_beam)
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=["test_begin", "TestOneRodBeamBendingForce::test_random_x2y"]
+    )
+    def test_random_z2x(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("z")
+            self.direction_force = self.get_random_unit_vector("x", self.direction_beam)
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=["test_begin", "TestOneRodBeamBendingForce::test_random_x2y"]
+    )
+    def test_random_z2y(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("z")
+            self.direction_force = self.get_random_unit_vector("y", self.direction_beam)
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_x2y",
+            "TestOneRodBeamBendingForce::test_random_x2z",
+        ]
+    )
+    def test_random_x2yz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("x")
+            self.direction_force = self.get_random_unit_vector(
+                "yz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_y2x",
+            "TestOneRodBeamBendingForce::test_random_y2z",
+        ]
+    )
+    def test_random_y2xz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("y")
+            self.direction_force = self.get_random_unit_vector(
+                "xz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_z2x",
+            "TestOneRodBeamBendingForce::test_random_z2y",
+        ]
+    )
+    def test_random_z2xy(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("z")
+            self.direction_force = self.get_random_unit_vector(
+                "xy", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_x2y",
+            "TestOneRodBeamBendingForce::test_random_y2x",
+        ]
+    )
+    def test_random_xy2xy(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("xy")
+            self.direction_force = self.get_random_unit_vector(
+                "xy", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_x2z",
+            "TestOneRodBeamBendingForce::test_random_z2x",
+        ]
+    )
+    def test_random_xz2xz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("xz")
+            self.direction_force = self.get_random_unit_vector(
+                "xz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_y2z",
+            "TestOneRodBeamBendingForce::test_random_z2y",
+        ]
+    )
+    def test_random_yz2yz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("yz")
+            self.direction_force = self.get_random_unit_vector(
+                "yz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_xy2xy",
+            "TestOneRodBeamBendingForce::test_random_x2yz",
+            "TestOneRodBeamBendingForce::test_random_y2xz",
+        ]
+    )
+    def test_random_xy2xyz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("xy")
+            self.direction_force = self.get_random_unit_vector(
+                "xyz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_yz2yz",
+            "TestOneRodBeamBendingForce::test_random_y2xz",
+            "TestOneRodBeamBendingForce::test_random_z2xy",
+        ]
+    )
+    def test_random_yz2xyz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("yz")
+            self.direction_force = self.get_random_unit_vector(
+                "xyz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_xz2xz",
+            "TestOneRodBeamBendingForce::test_random_x2yz",
+            "TestOneRodBeamBendingForce::test_random_z2xy",
+        ]
+    )
+    def test_random_xz2xyz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("xz")
+            self.direction_force = self.get_random_unit_vector(
+                "xyz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(
+        depends=[
+            "TestOneRodBeamBendingForce::test_random_xy2xyz",
+            "TestOneRodBeamBendingForce::test_random_xz2xyz",
+            "TestOneRodBeamBendingForce::test_random_xz2xyz",
+        ]
+    )
+    def test_random_xyz2xyz(self, ntests=10):
+        for i in range(ntests):
+            self.direction_beam = self.get_random_unit_vector("xyz")
+            self.direction_force = self.get_random_unit_vector(
+                "xyz", self.direction_beam
+            )
+            self.run_test()
+
+    @pytest.mark.order(6)
+    @pytest.mark.dependency(depends=["TestOneRodBeamBendingForce::test_random_xyz2xyz"])
+    def test_end(self):
+        pass
+
+
 @pytest.mark.order(6)
 @pytest.mark.dependency(
     depends=[
         "test_begin",
         "TestOneRodBeamTraction::test_end",
         "TestOneRodBeamTorsion::test_end",
+        "TestOneRodBeamBendingForce::test_end",
     ]
 )
 def test_end():
